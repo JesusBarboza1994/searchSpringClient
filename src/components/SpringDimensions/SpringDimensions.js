4fa30e9b35ad04fbf1dfe644b853e9083408016e
titaRuiz1 <asuntita.rl@gmail.com>
2023-08-23 18:37:59 -0500

    add:working progress page spring view

---
+++ src/components/SpringDimensions/SpringDimensions.js
@@ -0,0 +1,16 @@
+
+
+
+export default function SpringDimensions({title,text}){
+
+
+ return (
+  <>
+    <p>MEDIDAS</p>
+
+  </>
+ )
+
+ 
+
+}
--- src/pages/SpringView/SpringView.js
+++ src/pages/SpringView/SpringView.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react"
 import { getCode } from "../../services/codes-service"
-import { Wrapper } from "./styles"
+import { DivCode, DivImg, DivModel, ButtonWsp, Wrapper, DivSpring } from "./styles"
 import { useParams } from "react-router-dom";
+import { useAuth } from "../../context/auth-context";
+import {BsWhatsapp} from "react-icons/bs"
+import { colors } from "../../styles";
+import SpringDimensions from "../../components/SpringDimensions/SpringDimensions";
 
 export default function SpringView(){
   const { id } = useParams();
   const [spring, setSpring] = useState(null)
+  const {car} = useAuth();
+
   useEffect(()=>{
     getCode(id)
     .then((response) =>{
@@ -15,7 +21,33 @@ export default function SpringView(){
   },[])
   return(
     <Wrapper>
-      <p>{JSON.stringify(spring)}</p>
+    
+    {
+      spring && 
+      <>
+      <DivSpring>
+        <DivCode>{spring.code.osis_code}</DivCode>
+        <DivImg>
+          <img src={`https://drive.google.com/uc?export=view&id=${spring.code.img_url}`} alt="sin imagen"/>
+        </DivImg>
+        <DivModel>
+          <p>{spring.code.model}</p>
+          <p>año</p>
+          <p>{spring.code.position}</p>
+        </DivModel>
+        <ButtonWsp>
+          <BsWhatsapp style={{color:colors.white,fontSize:30}}/>
+          <p>HACER PEDIDO</p>
+        </ButtonWsp>
+        
+      </DivSpring>
+      <SpringDimensions/>
+      </>
+      
+    }
+    
+    <p> "SPRING:"{JSON.stringify(spring)}</p>
+    <p>"CAR:" {JSON.stringify(car)}</p>
     </Wrapper>
   )
 }
