7d45ba0910953fdb933c208b7bd89a2df4299648
Jesus Barboza <jbarz20@gmail.com>
2023-08-25 20:30:37 -0500

    CHORE: update SearchInterface page

--- src/pages/SearchInterface/styles.js
+++ src/pages/SearchInterface/styles.js
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import { colors } from "../../styles/colors";
-import { typography } from "../../styles";
 
 export const Wrapper = styled.div`
   background: lightgray;
@@ -16,44 +15,10 @@ export const CodeContainer = styled.div`
   gap:24px;
   padding: 24px;
 `
-
-export const Aside = styled.aside`
-  background: ${colors.white};
-  color: ${colors.black.light};
+export const Main = styled.div`
   display:flex;
   flex-direction:column;
-  padding: 12px 32px;
-  width:300px;
+  justify-content:center;  
 `
 
-export const FilterTitle = styled.p`
-  ${typography.head.xs};
-  border-bottom: 1px solid ${colors.black.dark};
-  margin-top:12px;
-`
 
-export const FilterContainer = styled.div`
-  width:100%;
-  display: flex;
-  flex-wrap:wrap;
-  gap:8px;
-  margin-bottom:8px;
-  margin-left:12px;
-`
-export const FilterOptionsTitle = styled.div`
-  ${typography.head.xxs};
-  margin-bottom:8px;
-  margin-top:16px;
-`
-
-export const CleanFilter = styled.p`
-  ${typography.head.xxs};   
-  text-decoration: underline;
-  cursor: pointer;
-`
-export const CheckBoxGroup = styled.div`
-  display:flex;
-  flex-direction:column;
-  gap: 2px;
-  margin-top:12px;
-`
