dfdf47c69cbc4de837332e3960f8f08dae3bffd1
jesus <jbarz20@gmail.com>
2023-08-29 01:06:02 -0500

    CHORE: available secure url api

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URI = "http://searchspringserver-production.up.railway.app/"
+export const BASE_URI = "https://searchspringserver-production.up.railway.app/"
 // export const BASE_URI = "http://127.0.0.1:3000/"
